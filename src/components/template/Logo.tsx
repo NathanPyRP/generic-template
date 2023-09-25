7a3b3514884a7da76a365a3d49fe6327a0fab496
NathanPyRP <nathanquirino123@gmail.com>
2023-09-25 12:01:12 -0300

    quarto commit

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -1,22 +1,35 @@
 export default function Logo() {
+    const circleStyles = {
+      background: "white",
+      width: "48px",
+      height: "48px",
+      borderRadius: "50%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    };
+  
     const gradient = "linear-gradient(135deg, #4F46E5, #C634E2)";
   
+    const textStyles = {
+      backgroundImage: gradient,
+      WebkitBackgroundClip: "text",
+      color: "transparent",
+      fontSize: "24px",
+      fontWeight: "bold", // Defina o peso da fonte aqui
+    };
+  
     return (
-      <div
-        className={`
-          flex flex-col items-center justify-center
-          h-12 w-12 rounded-full
-        `}
-        style={{ background: gradient }}
-      >
-        <div className="text-white font-bold text-lg">
-          <span style={{ backgroundImage: gradient, WebkitBackgroundClip: "text" }}>
+      <div className="flex items-center justify-center">
+        <div style={circleStyles}>
+          <div style={textStyles}>
             N
-          </span>
-          <span style={{ backgroundImage: gradient, WebkitBackgroundClip: "text" }}>
+          </div>
+          <div style={textStyles}>
             Q
-          </span>
+          </div>
         </div>
       </div>
     );
   }
+  
